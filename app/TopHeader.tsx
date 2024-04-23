6722a3cee2b478791a22fd530beb6ec789dc3828
sefaksoy <sefa_0638@hotmail.com>
2024-04-23 16:33:10 +0300

    top header end

--- app/TopHeader.tsx
+++ app/TopHeader.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrochip, faSearch, faTableList } from "@fortawesome/free-solid-svg-icons";
 
 export default function TopHeader() {
   return (
@@ -8,7 +10,7 @@ export default function TopHeader() {
       {/* Header Container */}
       <div className="container flex items-center justify-between space-x-16">
       {/* Logo */}
-      <a href="#" className='text-3xl font-bold'>DARK KNIGHT</a>
+      <a href="#" className='text-3xl font-bold text-transparent bg-gradient-to-r bg-clip-text from-red-500 to-white'>DARK KNIGHT</a>
       {/* Navigation */}
       <nav className='flex justify-between flex-1'>
         {/* Menu */}
@@ -24,12 +26,18 @@ export default function TopHeader() {
           {/* Search Area */}
           <form action="#">
             <div className='border-r px-4 mx-4 py-1 border-red-600'>
-                  <input type="text" className='bg-transparent border-b border-red-600 focus:outline-none w-44'/>
-                  <button><i className="fa-solid fa-user text-white"></i>tset</button>
-                 <FontAwesomeIcon icon={faHouse} />
+                  <input type="text" className='opacity-50 group-hover:opacity-100 bg-transparent border-b border-red-600 focus:outline-none w-44 transition duration-500'/>
                   
+                  <button className='-ml-4 group-hover:ml-0 transition duration-500'><FontAwesomeIcon icon={faSearch} className='group-hover:text-red transition duration-500' width={20} /></button>
+
             </div>
           </form>
+
+          {/* Sign Up Area */}
+          <div className='flex items-center space-x-8 text-lg'>
+            <a href="#">Login</a>
+            <a href="#" className='bg-red px-3 py-1 hover:bg-rose-600 text-white cursor-pointer transition duration-500'>Sign Up</a>
+          </div>
         </div>
       </nav>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@awesome.me/kit-d594b55eb6/icons/classic/solid';
+
 
 
 const inter = Inter({ subsets: ["latin"] });
